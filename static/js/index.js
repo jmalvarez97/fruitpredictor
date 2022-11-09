35f32ac78b7258aa082185ceb564197cd1d4cb3f
Juan Manuel Alvarez <jualvarez@fi.uba.ar>
2022-11-09 20:35:47 -0300

    add mimetype and /

--- static/js/index.js
+++
@@ -1,8 +0,0 @@
-
-const p = document.querySelector('p')
-window.addEventListener('scroll', () => {
-    const current = window.scrollY
-    console.log(current)
-    p.style.color =  `rgb(255,255,255,clamp(0.2, ${current}, 0.7))`
-
-})
