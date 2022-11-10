f3864e6e2c9368a9d47b387acb5355168655aeb5
Juan Manuel Alvarez <jualvarez@fi.uba.ar>
2022-11-10 12:16:37 -0300

    cambios en models

--- mlmodel/models.py
+++ mlmodel/models.py
@@ -4,9 +4,9 @@ from django.db import models
 # Create your models here.
 
 class Images(models.Model):
-    name = models.CharField(max_length=20)
-    image = models.CharField(max_length=40000)
-    predict = models.CharField(max_length=20)
+    name = models.CharField(max_length=80)
+    image = models.CharField(max_length=100000)
+    predict = models.CharField(max_length=80)
 
     def __str__(self):
         return self.name
