78bcc8e04a3e16846d1d6757e8376485b1499132
Juan Manuel Alvarez <jualvarez@fi.uba.ar>
2022-11-10 16:37:31 -0300

    cambios en front

--- mlmodel/static/js/index.js
+++ mlmodel/static/js/index.js
@@ -1,8 +1,13 @@
-
+const text = document.querySelector('h2')
+window.addEventListener('scroll' , () => {
+const current = window.scrollY;
+if (window.screen.width > 520){
+    text.style.fontSize = `clamp(4rem, ${current}px , 5rem)`
+}
+})
 const p = document.querySelector('p')
 window.addEventListener('scroll', () => {
     const current = window.scrollY
-    console.log(current)
     p.style.color =  `rgb(255,255,255,clamp(0.2, ${current}, 0.7))`
-
 })
+
