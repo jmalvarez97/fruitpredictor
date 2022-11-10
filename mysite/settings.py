cd8a51df75e8778d407e44d6ec6b69c3341a70ee
Juan Manuel Alvarez <jualvarez@fi.uba.ar>
2022-11-10 09:49:39 -0300

    add csfr

--- mysite/settings.py
+++ mysite/settings.py
@@ -137,4 +137,7 @@ STATIC_URL = "static/"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
+
+CSRF_TRUSTED_ORIGINS = ['https://django-server-production-b4b5.up.railway.app']
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
